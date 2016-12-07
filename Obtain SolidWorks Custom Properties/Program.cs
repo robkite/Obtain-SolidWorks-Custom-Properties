2702fe884d3c2eadfff90d55f5a5510936154e43
robkite <rkite89@gmail.com>
2016-12-07 09:21:12 +0000

    Add project files.

---
+++ Obtain SolidWorks Custom Properties/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obtain_SolidWorks_Custom_Properties {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
